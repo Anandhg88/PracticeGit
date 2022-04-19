c6579ba4d4687f0201037ecd4dc3cfba2eed90fc
Anandhg88 <anandhg88@gmail.com>
2022-04-19 12:00:39 +0530

    second commit

--- src/test/java/org/test/NewProject/Sample.java
+++ src/test/java/org/test/NewProject/Sample.java
@@ -3,7 +3,8 @@ package org.test.NewProject;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("this is test");	
+		System.out.println("this is test");
+		System.out.println("this is test11");
 		
 	}
 }
