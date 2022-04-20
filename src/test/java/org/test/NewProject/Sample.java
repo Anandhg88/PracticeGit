f8917b6180a67c39bfe357f098a5e107bcfdfd09
Anandhg88 <anandhg88@gmail.com>
2022-04-20 22:59:59 +0530

    Sathiesh code

--- src/test/java/org/test/NewProject/Sample.java
+++ src/test/java/org/test/NewProject/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("this is test2");
 		System.out.println("this is test11");
-		System.out.println("this is test 13");
+		System.out.println("this is test 23");
 		System.out.println("this is test 14");
 		System.out.println("this is test 7");
 		System.out.println("this is test 8");
