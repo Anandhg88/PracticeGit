85f734f7a3e034771b129dad0a2ea623ed6822e8
Anandhg88 <anandhg88@gmail.com>
2022-04-20 22:39:57 +0530

    Sathiesh's code

--- src/test/java/org/test/NewProject/Sample.java
+++ src/test/java/org/test/NewProject/Sample.java
@@ -3,7 +3,7 @@ package org.test.NewProject;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("this is test");
+		System.out.println("this is test2");
 		System.out.println("this is test11");
 		System.out.println("this is test 3");
 		System.out.println("this is test 4");
