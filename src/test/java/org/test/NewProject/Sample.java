cc2e26052a468ba3c002b32414fea2e41266063c
Anandhg88 <anandhg88@gmail.com>
2023-02-13 12:48:45 +0530

    Anandh commit today

--- src/test/java/org/test/NewProject/Sample.java
+++ src/test/java/org/test/NewProject/Sample.java
@@ -3,10 +3,10 @@ package org.test.NewProject;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("this is test");
-		System.out.println("this is test11");
-		System.out.println("this is test 113");
-		System.out.println("this is test 224");
+		System.out.println("this is test 1");
+		System.out.println("this is test 2");
+		System.out.println("this is test 3");
+		System.out.println("this is test 4");
 		
 	}
 }
